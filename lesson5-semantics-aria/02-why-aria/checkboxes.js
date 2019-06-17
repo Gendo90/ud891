794d412c19a7ed678ff44214379b7116f9f9a2ba
Patrick Gendotti <patrick.gendotti@gmail.com>
2019-06-17 15:49:14 -0700

    chore: Update exercise files to make website more accessible to screenreaders using aria "role" and "aria-checked" features

--- lesson5-semantics-aria/02-why-aria/checkboxes.js
+++ lesson5-semantics-aria/02-why-aria/checkboxes.js
@@ -32,10 +32,16 @@
   };
 
   Checkbox.prototype.toggle = function() {
-    if (this.el.hasAttribute('checked'))
-      this.el.removeAttribute('checked');
-    else
-      this.el.setAttribute('checked', '');
+    if (this.el.hasAttribute('checked')) {
+        this.el.removeAttribute('checked');
+        // this.el.setAttribute('aria-checked') = false;
+        this.el.setAttribute('aria-checked', "false");
+    }
+    else {
+        this.el.setAttribute('checked', "checked");
+        this.el.setAttribute('aria-checked', "true");
+        // this.el.setAttribute('aria-checked') = true;
+    }
   };
 
   var checkboxes = slice(document.querySelectorAll('.checkbox'));
