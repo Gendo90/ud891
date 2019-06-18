9962e19b340c445414e87f29d53c11f8af4bc4f5
Patrick Gendotti <patrick.gendotti@gmail.com>
2019-06-18 13:11:41 -0700

    fix: Announce dialog box when modal is opened, and stop tab from focusing on elements outside of the modal when open

--- lesson5-semantics-aria/21-dialog/modal.js
+++ lesson5-semantics-aria/21-dialog/modal.js
@@ -34,7 +34,8 @@ function openModal(e) {
   modal.style.display = 'block';
   modalOverlay.style.display = 'block';
 
-  // Focus first child
+  // Focus header
+  // document.getElementById("login").focus();
   firstTabStop.focus();
 
   function trapTabKey(e) {
@@ -64,6 +65,7 @@ function openModal(e) {
   }
 
   // FIXME: hide non-modal content from screen readers
+  document.querySelector(".wrapper").setAttribute("aria-hidden", "true")
 }
 
 function closeModal() {
@@ -75,4 +77,6 @@ function closeModal() {
   focusedElementBeforeModal.focus();
 
   // FIXME: don't forget to make main content screen reader accessible again.
+  document.querySelector(".wrapper").removeAttribute("aria-hidden")
+
 }
